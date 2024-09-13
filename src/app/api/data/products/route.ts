62d9130e092b92f72027d607ca6b28d400b6d1c5
EuMotta <hyperxmotta@hotmail.com>
2024-09-13 17:10:03 -0300

    fix: erro no searchParams

--- src/app/api/data/products/route.ts
+++ src/app/api/data/products/route.ts
@@ -1,23 +1,21 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import { ProductsService } from '../../services/product';
 
 const product = new ProductsService();
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const page = searchParams.get('page')
+    ? parseInt(searchParams.get('page')!, 10)
+    : 1;
+  const category = searchParams.get('category') || '';
+  const sub_category = searchParams.get('sub_category') || '';
+  const limit = searchParams.get('per_page')
+    ? parseInt(searchParams.get('per_page')!, 10)
+    : 10;
+
   try {
-    const { searchParams } = new URL(request.url);
-
-    const page = searchParams.get('page')
-      ? parseInt(searchParams.get('page')!, 10)
-      : 1;
-    const category = searchParams.get('category') || '';
-    const sub_category = searchParams.get('sub_category') || '';
-
-    const limit = searchParams.get('per_page')
-      ? parseInt(searchParams.get('per_page')!, 10)
-      : 10;
-
     const offset = (page - 1) * limit;
 
     const products = await product.getProducts({
